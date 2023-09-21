c8d48bd55bc2d647a97132aa469bec75f30ef4e7
jeongwooHam <hamjw0122@gmail.com>
2023-09-21 10:52:41 +0900

    chore: 불필요한 코드 제거

--- 🤷‍♀️How Can I Use/3. google-map-api/src/pages/index.tsx
+++ 🤷‍♀️How Can I Use/3. google-map-api/src/pages/index.tsx
@@ -1,9 +1,8 @@
 import { Wrapper } from "@googlemaps/react-wrapper";
 import Map from "../components/map";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Marker from "../components/marker";
 import SearchBar from "../components/search-bar";
-import { getLocation } from "../apis/reverseGeocode";
 
 const Main: React.FC = () => {
   const apiKey = process.env.REACT_APP_KEY;
