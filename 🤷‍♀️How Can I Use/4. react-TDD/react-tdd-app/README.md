32b2178c3ad3320d9a1776e2029a2776eb972437
JeongwooHam <hamjw0122@gmail.com>
2023-11-29 00:16:28 +0900

    🎨 style: prettier, eslint 적용

--- 🤷‍♀️How Can I Use/4. react-TDD/react-tdd-app/README.md
+++ 🤷‍♀️How Can I Use/4. react-TDD/react-tdd-app/README.md
@@ -1,12 +1,12 @@
 ### REACT TDD
 
-1. CRA로 React 애플리케이션을 생성한다.
+1. CRA로 React 애플리케이션 생성
 
 ```
 npx create-react-app react-tdd-app
 ```
 
-2. 애플리케이션 실행 후 기본 테스트를 진행한다.
+2. 애플리케이션 실행 후 기본 테스트를 진행
 
 ```
 npm test
@@ -14,3 +14,25 @@ npm test
 
 - a를 누르면 테스트가 시작된다.
 - q를 누르면 테스트를 중단할 수 있다.
+
+3. Prettier, ESLint 설치 및 설정
+
+- ESLint Testing Plugins 설치하기
+
+```
+npm install -D eslint-plugin-testing-library eslint-plugin-jest-dom
+```
+
+- 내부 설정해주기
+
+```js
+plugins: ['react', 'testing-library', 'jest-dom'],
+extends: [
+	'react-app',
+	'react-app/jest',
+	'plugin:testing-library/react',
+	'plugin:jest-dom/recommended',
+],
+```
+
+### 추가로 사용한 스택
--- 🤷‍♀️How Can I Use/4. react-TDD/react-tdd-app/src/App.test.js
+++ 🤷‍♀️How Can I Use/4. react-TDD/react-tdd-app/src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+	render(<App />)
+	const linkElement = screen.getByText(/learn react/i)
+	expect(linkElement).toBeInTheDocument()
+
+	const lintTest = screen.getByRole('button', { name: 'lintTest' })
+
+	// ESLint 적용 확인
+	// expect(lintTest.textContent).toBe('lintTest')
+	expect(lintTest).toHaveTextContent('lintTest')
+})
